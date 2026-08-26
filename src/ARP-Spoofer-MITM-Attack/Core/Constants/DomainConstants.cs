d07e0c0d943f20735e77c099d115ec2775be4a62
ViceCraftsman <notissuperphilps@gmail.com>
2026-08-26 11:03:37 -0700

    Initial commit: src/ARP-Spoofer-MITM-Attack/Core/Constants/DomainConstants.cs

---
+++ src/ARP-Spoofer-MITM-Attack/Core/Constants/DomainConstants.cs
@@ -0,0 +1,15 @@
+namespace ARPSpooferMITMAttack.Core.Constants
+{
+    public static class DomainConstants
+    {
+        public const int DefaultPageSize = 50;
+        public const int MaxPageSize = 1000;
+        public const int DefaultRetryCount = 3;
+        public const int DefaultTimeoutMs = 30000;
+        public const string DefaultLogLevel = "Information";
+        public const string ConfigurationSectionName = "ARPSpooferMITMAttack";
+        public const string HealthCheckEndpoint = "/health";
+        public const string MetricsEndpoint = "/metrics";
+        public const int CacheExpirationMinutes = 5;
+    }
+}
