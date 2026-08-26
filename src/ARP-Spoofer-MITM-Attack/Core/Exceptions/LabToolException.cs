c61dc1bbdaa7701e9b065c2e744200c0d8edf472
ViceCraftsman <notissuperphilps@gmail.com>
2026-08-26 11:03:43 -0700

    Initial commit: src/ARP-Spoofer-MITM-Attack/Core/Exceptions/LabToolException.cs

---
+++ src/ARP-Spoofer-MITM-Attack/Core/Exceptions/LabToolException.cs
@@ -0,0 +1,7 @@
+namespace ARPSpooferMITMAttack.Core.Exceptions
+{
+    public class LabToolException : Exception
+    {
+        public LabToolException(string message) : base(message) { }
+    }
+}
