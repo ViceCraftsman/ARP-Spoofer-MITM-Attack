b24b673bb34e6d5e3de41bc5a76c7ef6f4df0879
ViceCraftsman <notissuperphilps@gmail.com>
2026-08-26 11:04:44 -0700

    Initial commit: src/ARP-Spoofer-MITM-Attack/Core/Utils/Utils.cs

---
+++ src/ARP-Spoofer-MITM-Attack/Core/Utils/Utils.cs
@@ -0,0 +1,21 @@
+namespace ARPSpooferMITMAttack.Core.Utils
+{
+    public static class ValidationUtils
+    {
+        public static bool IsValidTarget(string target) => !string.IsNullOrWhiteSpace(target) && target.Length <= 256;
+    }
+
+    public static class ArgumentParser
+    {
+        public static Dictionary<string, string> Parse(string[] args)
+        {
+            var result = new Dictionary<string, string>();
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].StartsWith("--") && i + 1 < args.Length) { result[args[i][2..]] = args[i + 1]; i++; }
+                else if (args[i].StartsWith("-") && i + 1 < args.Length) { result[args[i][1..]] = args[i + 1]; i++; }
+            }
+            return result;
+        }
+    }
+}
