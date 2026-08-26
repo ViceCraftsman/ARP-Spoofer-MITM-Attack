239b1ee20172fe2a88ae83f2d7c23488c4282928
ViceCraftsman <notissuperphilps@gmail.com>
2026-08-26 11:03:08 -0700

    Initial commit: tests/QueueTests.cs

---
+++ tests/QueueTests.cs
@@ -0,0 +1,20 @@
+using ARPSpooferMITMAttack.Core.Models;
+using ARPSpooferMITMAttack.Infrastructure.Queue;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace ARPSpooferMITMAttack.Tests
+{
+    public class QueueTests
+    {
+        [Fact]
+        public async Task JobQueue_EnqueueAndDequeue()
+        {
+            var queue = new InMemoryJobQueue(NullLogger<InMemoryJobQueue>.Instance);
+            await queue.EnqueueAsync(new Job { Name = "test" });
+            var job = await queue.DequeueAsync();
+            Assert.NotNull(job);
+            Assert.Equal("test", job.Name);
+        }
+    }
+}
